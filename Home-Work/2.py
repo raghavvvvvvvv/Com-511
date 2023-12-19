59abfb992c6c2255fb56bdb0ee4859af0af8b05d
raghavvvvvvvv <138058029+raghavvvvvvvv@users.noreply.github.com>
2023-12-19 20:14:14 +0530

    Update 2.py

--- Home-Work/2.py
+++ Home-Work/2.py
@@ -1,1 +1,11 @@
-
+# Dictionary
+dict = {}
+n = int(input())
+for i in range(n):
+    key = input()
+    value  = input()
+    dict[key] = value
+for i in  dict.keys():
+    print(i, end=" ")
+for i in  dict.values():
+    print(i, end=" ")
