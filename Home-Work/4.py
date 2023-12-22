4f5eee6a229799e4b9a9d7ba314ac0adcca1365c
raghavvvvvvvv <138058029+raghavvvvvvvv@users.noreply.github.com>
2023-12-22 23:09:40 +0530

    Update 4.py

--- Home-Work/4.py
+++ Home-Work/4.py
@@ -1,1 +1,13 @@
 
+
+# unique values 
+list = []
+n = int(input())
+
+for i in range (n):
+    add = int (input())
+    list.append(add)
+unique = set (list)
+print("unique values are :")
+for i in unique :
+    print(i)
